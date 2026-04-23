928008fee910be26ae6dd48ba31189d488a9afdb
9thIvy <180355498+9thIvy@users.noreply.github.com>
2026-04-23 12:27:15 -0700

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+#[macro_use] extern crate rocket;
+mod paste_id;
+use paste_id::PasteId;
+use rocket::tokio::fs::File;
+use rocket::data::{Data, ToByteUnit};
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, retrieve, upload])
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "This is some text!"
+}
+
+#[get("/<id>")]
+async fn retrieve(id: PasteId<'_>) -> Option<File>{
+    File::open(id.file_path()).await.ok()
+}
+
+#[post("/", data = "<paste>")]
+async fn upload(paste: Data<'_>) -> std::io::Result<String>{
+    let id = PasteId::new();
+    paste.open(128.kibibytes()).into_file(id.file_path()).await?;
+    // TODO: return full path. Wait until dns is done
+    // also use uri!()
+    Ok(id.to_string())
+}
---
+++ src/paste_id.rs
@@ -0,0 +1,37 @@
+use rocket::request::FromParam;
+use std::borrow::Cow;
+use std::path::{Path, PathBuf};
+use uuid::Uuid;
+
+pub struct PasteId<'a>(Cow<'a, str>);
+
+impl<'a> PasteId<'_> {
+    pub fn new() -> PasteId<'a> {
+        let id = Uuid::new_v4();
+        PasteId(Cow::Owned(id.to_string()))
+    }
+
+    pub fn file_path(&self) -> PathBuf {
+        let root = concat!(env!("CARGO_MANIFEST_DIR"), "/", "upload");
+        let filename = self.0.to_string();
+        Path::new(root).join(filename)
+    }
+}
+
+impl<'a> FromParam<'a> for PasteId<'a> {
+    type Error = &'a str;
+
+    fn from_param(param: &'a str) -> Result<Self, Self::Error> {
+        match Uuid::parse_str(param){
+            Ok(_) => Ok(PasteId(Cow::Owned(param.to_string()))),
+            Err(_) => Err("")
+        }
+
+    }
+}
+
+impl<'a> std::fmt::Display for PasteId<'a>{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
