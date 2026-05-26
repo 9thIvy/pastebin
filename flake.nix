b8bb5d89385c879ee56e6587f3da1bb9350d3f13
9thIvy <180355498+9thIvy@users.noreply.github.com>
2026-05-26 15:54:16 -0700

    testing ai slop because im at a loss

--- flake.nix
+++ flake.nix
@@ -1,27 +1,26 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.05";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    flake-parts,
-  }:
-  flake-parts.lib.mkFlake { inherit inputs; }
-  {
-    imports = [
-      ./default.nix
-      ./nix/nixos.nix
-      ./nix/package.nix
-    ];
+  # probably ai garbage
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: 
+      let 
+        pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
+        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
 
-    overlays = {
-      pineapple_pastebin = import ./nix/overlay.nix;
-    };
-    
-  };
+        pineapple_pastebin = pkgs.rustPlatform.buildRustPackage {
+          pname = manifest.name;
+          version = manifest.version;
+          cargoLock.lockFile = ./Cargo.lock;
+          src = pkgs.lib.cleanSource ./.;
+        };
+      in {
+        packages.default = pineapple_pastebin;
 
-  
+        nixosModules.default = (import ./nix/pineapple_pastebin.nix { inherit pkgs lib; pineapple_pastebin = pineapple_pastebin; });
+      });
 }
